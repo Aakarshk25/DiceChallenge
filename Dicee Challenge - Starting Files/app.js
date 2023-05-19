b50ae317f0b6075b30995ae47f1d4299f316db18
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2023-05-19 06:36:30 +0530

    Update app.js

--- Dicee Challenge - Starting Files/app.js
+++ Dicee Challenge - Starting Files/app.js
@@ -1,24 +1,27 @@
-var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomNumber1, randomNumber2;
 
-var source = "images/" + randomDiceImage;
-var iamge1 = document.querySelectorAll("img")[0].setAttribute("src", source);
+function rollDice() {
+  randomNumber1 = Math.floor((Math.random() * 6) + 1);
+  var randomDiceImage = "dice" + randomNumber1 + ".png";
+  var source = "images/" + randomDiceImage;
+  document.querySelectorAll("img")[0].setAttribute("src", source);
 
-var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+  randomNumber2 = Math.floor((Math.random() * 6) + 1);
+  var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
-var imge1 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
-
-if(randomNumber1 == randomNumber2){
-    document.querySelector("h1").innerHTML = "shit its a tie fuck" 
-} 
-
-if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "player 2 wins"
-    
-} else {
-    document.querySelector("h1").innerHTML = "player 1 wins"
+  if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").innerHTML = "Tie";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+  } else {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+  }
 }
+
+const rollDiceBtn = document.querySelector(".btn");
+rollDiceBtn.addEventListener('click', rollDice);
+
 
 
 
